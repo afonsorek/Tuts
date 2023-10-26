8ee319d188c6d927beb24c159cce1a7bcdedc20b
Afonso Rekbaim <afonso.rekbaim@pucpr.edu.br>
2023-10-26 18:37:32 -0300

    Initial Commit

---
+++ BeatsTest/BeatsTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeatsTestApp.swift
+//  BeatsTest
+//
+//  Created by Afonso Rekbaim on 26/10/23.
+//
+
+import SwiftUI
+
+@main
+struct BeatsTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

73776255523f98dda8caa474707481de132424a9
Rafa Teivfik <rafael.teivfik2@gmail.com>
2023-11-13 16:46:42 -0300

    Adicionadas opçõesde configurações: showBeats, noteColors, colorblindAccessibility e metronomeActive (#8)

--- BeatsTest/Controllers/ConfigController.swift
+++ BeatsTest/Controllers/ConfigController.swift
@@ -17,6 +17,10 @@ class ConfigController : ObservableObject {
     @Published var config = Config()
     
     var orientationBinding : Binding<Bool> = Binding(get: {false}, set: {_ in })
+    var showBeatsBinding : Binding<Bool> = Binding(get: {false}, set: {_ in })
+    var noteColorsBinding : Binding<Bool> = Binding(get: {false}, set: {_ in })
+    var colorblindAccessibilityBinding : Binding<Bool> = Binding(get: {false}, set: {_ in })
+    var metronomeActiveBinding : Binding<Bool> = Binding(get: {false}, set: {_ in })
     
     // Event functions
     init() {
@@ -33,12 +37,14 @@ class ConfigController : ObservableObject {
                 }
             }
         )
+        showBeatsBinding = Binding(get: {self.config.showBeats}, set: {self.config.showBeats = $0})
+        noteColorsBinding = Binding(get: {self.config.noteColors}, set: {self.config.noteColors = $0})
+        colorblindAccessibilityBinding = Binding(get: {self.config.colorblindAccessibility}, set: {self.config.colorblindAccessibility = $0})
+        metronomeActiveBinding = Binding(get: {self.config.metronomeActive}, set: {self.config.metronomeActive = $0})
     }
     
     // Public functions
     func forceScreenOrientation() {
-//        let value = UIInterfaceOrientation.landscapeRight.rawValue
-//        UIDevice.current.setValue(value, forKey: "orientation")
         guard let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene else { return }
         windowScene.requestGeometryUpdate(.iOS(interfaceOrientations: .landscape))
     }
--- BeatsTest/Models/ConfigModel.swift
+++ BeatsTest/Models/ConfigModel.swift
@@ -9,4 +9,8 @@ import Foundation
 
 class Config : ObservableObject {
     @Published var orientationLock : Bool = false
+    @Published var showBeats : Bool = true
+    @Published var noteColors : Bool = true
+    @Published var colorblindAccessibility : Bool = false
+    @Published var metronomeActive : Bool = false
 }

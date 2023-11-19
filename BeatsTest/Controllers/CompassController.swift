a9a051b890da462e4c2d44bd7d737d3a5511014a
Afonso Rekbaim <afonso.rekbaim@pucpr.edu.br>
2023-11-19 15:45:47 -0300

    Configurações implementadas (menos daltonismo e metronomo) (#20)
    Co-authored-by: Rafael Teivfik MacOS <rafael.teivfik2@gmail.com>

--- BeatsTest/Controllers/CompassController.swift
+++ BeatsTest/Controllers/CompassController.swift
@@ -20,7 +20,8 @@ class CompassController : ObservableObject {
     var pulseCountBinding : Binding<Int> = Binding ( get: {-1}, set: {_ in })
     var pulseDurationBinding : Binding<Int> = Binding ( get: {-1}, set: {_ in })
     
-    init() {
+    init(compass : Compass  = Compass(pulseCount: 4, pulseDuration: 4, notes: [])) {
+        self.compass = compass
         time.timerListeners.append({beat in
             if beat < 0 {
                 self.looped = false
@@ -105,6 +106,10 @@ class CompassController : ObservableObject {
     
     func removeAllNotes(){
         compass.notes.removeAll()
+        objectWillChange.send()
+    }
+    
+    func updateNotes(){
         objectWillChange.send()
     }
     

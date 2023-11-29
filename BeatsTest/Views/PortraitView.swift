0c38c70ec520a2457af470e03268819a2638242a
Rafa Teivfik <rafael.teivfik2@gmail.com>
2023-11-29 15:58:37 -0300

    Random (#23)
    * Adicionada funcionalidade de ativar e desativar o loop de compasso
    
    * Adicionada funcionalidade de aleatorizacao, mas com bug de visualizacao
    
    * Consertado aleatorio nao atualizando as notas corretamente

--- BeatsTest/Views/PortraitView.swift
+++ BeatsTest/Views/PortraitView.swift
@@ -27,9 +27,9 @@ struct PortraitView: View {
     
     @State var buttonScaleEffect = 1.0
     @State var buttonScaleEffect2 = 1.0
+    @State var buttonScaleEffect3 = 1.0
     @State var scaleAnimation = 1.0
     @State var scaleAnimation2 = 1.0
-        
     var body: some View {
         ZStack{
             VStack{
@@ -168,10 +168,38 @@ struct PortraitView: View {
                                 }
                             }
                             .overlay(
-                            RoundedRectangle(cornerRadius: 16)
-                            .inset(by: 0.5)
-                            .stroke(.white, lineWidth: 1)
-                            .scaleEffect(CGSize(width: buttonScaleEffect2, height: buttonScaleEffect2))
+                                RoundedRectangle(cornerRadius: 16)
+                                .inset(by: 0.5)
+                                .stroke(.white, lineWidth: 1)
+                                .scaleEffect(CGSize(width: buttonScaleEffect2, height: buttonScaleEffect2))
+                            )
+                            
+                            ZStack{
+                                HStack{
+                                    Image(systemName: "gift")
+                                        .font(.title3)
+                                        .bold()
+                                        .foregroundStyle(.white)
+                                }
+                                .padding(.horizontal, 13)
+                                .padding(.vertical, 11)
+                            }
+                            .scaleEffect(CGSize(width: buttonScaleEffect3, height: buttonScaleEffect3))
+                            .clipShape(RoundedRectangle(cornerSize: CGSize(width: 16, height: 16)))
+                            .onTapGesture {
+                                compassController.randomizeNotes()
+                                withAnimation(.linear(duration: 0.3)){
+                                    buttonScaleEffect3 -= 0.4
+                                    withAnimation(.easeOut(duration: 0.3)){
+                                        buttonScaleEffect3 = 1.0
+                                    }
+                                }
+                            }
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 16)
+                                .inset(by: 0.5)
+                                .stroke(.white, lineWidth: 1)
+                                .scaleEffect(CGSize(width: buttonScaleEffect2, height: buttonScaleEffect2))
                             )
                             Spacer()
                             ZStack{
